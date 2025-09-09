7d4a4e786e7dfa2bd986084739dc3985039a1db9
Mauro Alejandro Gerardi <mgerardi@leafnoise.io>
2025-09-09 16:35:05 -0300

    fix: Update EmailJS environment variable references to VITE prefix

--- src/containers/contexts/contactContext.tsx
+++ src/containers/contexts/contactContext.tsx
@@ -35,10 +35,10 @@ export const ContactProvider = ({ children }: ContactContextPropsType) => {
 
     try {
       const result = await emailjs.send(
-        import.meta.env.REACT_APP_EMAILJS_SERVICE_ID!,
-        import.meta.env.REACT_APP_EMAILJS_TEMPLATE_ID!,
+        import.meta.env.VITE_EMAILJS_SERVICE_ID!,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID!,
         formData,
-        import.meta.env.REACT_APP_EMAILJS_PUBLIC_KEY!
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY!
       );
       console.log("Email sent", result.text);
 
